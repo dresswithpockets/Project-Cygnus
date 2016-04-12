cb1b6619a4cc0127c717d65dc67f76371904c9c7
Tristen Horton <tristen@tristenhorton.com>
2016-04-12 03:54:48 -0700

    Template API class for consumable items (food)

---
+++ Assets/Scripts/API/Items/ConsumableTemplate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Consumable_Template : Item_Template {
+
+	private Consumable m_consumable_object;
+	public Consumable consumable_object {
+		
+		get {
+
+			return m_consumable_object;
+		}
+		internal set {
+
+			m_consumable_object = value;
+		}
+	}
+
+	public abstract bool pets_can_eat { get; }
+
+	public virtual void consume(Player_Controller player) { }
+
+	public virtual void consume(NPC npc) { }
+
+	public virtual void consume(Pet_Item_Template pet) { } // TODO: Once Pet_Item MonoBehaviour is implemented, change this param type to Pet_Item.
+}
