018cfaba16975c7bb667942dd753c497940d5136
Ashley <dresswithpockets@pm.me>
2016-04-12 04:24:17 -0700

    New folder structure.
    Organization, thats why!

---
+++ Assets/Scripts/Game/Items/Material.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Material : MonoBehaviour {
+
+	public MeshFilter filter;
+
+	private Material_Template m_template = null;
+
+	public Material_Template template {
+
+		get {
+
+			return m_template;
+		}
+		internal set {
+
+			m_template.owner = gameObject;
+			m_template.material_object = this;
+		}
+	}
+
+	#region Ownership
+
+	private NPC m_NPC_owner = null;
+	private Player_Controller m_player_owner = null;
+
+
+	public NPC NPC_owner {
+
+		get {
+
+			return m_NPC_owner;
+		}
+		internal set {
+
+			m_NPC_owner = value;
+		}
+	}
+
+	public Player_Controller player_owner {
+
+		get {
+
+			return m_player_owner;
+		}
+		internal set {
+
+			m_player_owner = value;
+		}
+	}
+
+	public Item_Owner ownership {
+
+		get {
+
+			return (NPC_owner == null ? (player_owner == null ? Item_Owner.NONE : Item_Owner.PLAYER) : Item_Owner.NPC);
+		}
+	}
+
+	internal void set_owner(Player_Controller player) {
+
+		NPC_owner = null;
+		player_owner = player;
+	}
+
+	internal void set_owner(NPC npc) {
+
+		player_owner = null;
+		NPC_owner = npc;
+	}
+
+	#endregion
+
+	void Start() {
+
+		filter = GetComponent<MeshFilter>();
+		template.spawned();
+	}
+
+	void Update() {
+
+		template.exists_update();
+
+		switch (ownership) {
+			case Item_Owner.NPC:
+
+				template.passive_update(NPC_owner);
+
+				break;
+			case Item_Owner.PLAYER:
+
+				template.passive_update(player_owner);
+
+				break;
+		}
+	}
+
+	void FixedUpdate() {
+
+		template.fixed_update();
+	}
+
+	void LateUpdate() {
+
+		template.late_update();
+	}
+
+	public void pickup() {
+
+		switch (ownership) {
+
+			case Item_Owner.NONE:
+
+				Debug.LogError("Cannot pick up item because no owner was assigned before the event was completed.", this);
+
+				break;
+			case Item_Owner.NPC:
+
+				template.picked_up(NPC_owner);
+
+				break;
+			case Item_Owner.PLAYER:
+				template.picked_up(player_owner);
+				break;
+		}
+	}
+
+	public void drop() {
+
+		switch (ownership) {
+
+			case Item_Owner.NONE:
+
+				Debug.LogError("Cannot drop item as no NPC or Player owns this item.", this);
+
+				break;
+			case Item_Owner.NPC:
+
+				template.dropped(NPC_owner);
+
+				break;
+			case Item_Owner.PLAYER:
+
+				template.dropped(player_owner);
+
+				break;
+		}
+	}
+
+	public void assign_template(Material_Template template, MeshFilter model) {
+
+		this.template = template;
+		filter.mesh = model.sharedMesh;
+	}
+}
---
+++ Assets/Scripts/Game/Items/Pet_Item.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// TODO: Implement Pet Items and Pets
+
+public class Pet_Item : MonoBehaviour {
+
+	private MeshFilter m_filter;
+	private bool m_is_item = true;
+	private Pet_Item_Template m_template = null;
+
+	public Pet_Item_Template template {
+		get {
+
+			return m_template;
+		}
+		internal set {
+
+			m_template = value;
+		}
+	}
+
+	public bool is_item {
+
+		get {
+
+			return m_is_item;
+		}
+		internal set {
+
+			m_is_item = value;
+		}
+	}
+
+	public MeshFilter filter {
+		get {
+			return m_filter;
+		}
+		set {
+			m_filter = value;
+		}
+	}
+
+	public void assign_template(Pet_Item_Template template, MeshFilter model) {
+
+		this.template = template;
+		filter.mesh = model.sharedMesh;
+	}
+}
