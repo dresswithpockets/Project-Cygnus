77322fb3c9b753c39f5f35f1ea0b34ef73f3a37c
Tristen Horton <tristen@tristenhorton.com>
2016-04-13 01:32:06 -0700

    Implemented give_ functions for items as well as partial stat manipulation via items in inventory.

--- Assets/Scripts/Game/Player_Controller.cs
+++ Assets/Scripts/Game/Player_Controller.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-// TODO: Implement the player's ability to craft items, equipment, etc.
-public class Player_Controller : MonoBehaviour {
+// TODO: Implement the player's ability to craft items, armor, etc.
+public sealed class Player_Controller : MonoBehaviour {
+
 	#region Stats
 
 	private bool m_is_alive = false;
@@ -15,6 +16,8 @@ public class Player_Controller : MonoBehaviour {
 	private float m_max_hp_default = 100f; // hp stat by default (at lowest hp level)
 	private float m_hp_regen = 0f; // Reg stat
 	private float m_attack_tempo = 1.0f; // Tempo stat
+	
+	private float m_hp_level = 0;
 
 	public float hp {
 
@@ -97,6 +100,25 @@ public class Player_Controller : MonoBehaviour {
 		}
 	}
 
+	public float default_max_hp {
+
+		get {
+			return m_max_hp_default;
+		}
+		internal set {
+			m_max_hp_default = value;
+		}
+	}
+
+	public float hp_level {
+		get {
+			return m_hp_level;
+		}
+		internal set {
+			m_hp_level = value;
+		}
+	}
+
 	public float hp_regen {
 
 		get {
@@ -118,6 +140,14 @@ public class Player_Controller : MonoBehaviour {
 		internal set {
 
 			m_attack_tempo = value;
+		}
+	}
+
+	public int player_level {
+
+		get {
+
+			return inventory.learned_ability_list.Count + 1;
 		}
 	}
 
@@ -198,6 +228,10 @@ public class Player_Controller : MonoBehaviour {
 		orbit_camera = GetComponentInChildren<UltimateOrbitCamera>().gameObject;
 		fp_controller = GetComponent<vp_FPController>();
 		inventory = GetComponent<Inventory_Controller>();
+
+		inventory.is_player_inventory = true;
+		inventory.player = this;
+
 		spawn(transform.position);
 	}
 
@@ -384,7 +418,91 @@ public class Player_Controller : MonoBehaviour {
 
 	public void update_stats() {
 
-		// TODO: Update stats based on weapons and inventory.
+		// TODO: determine a more realistic interval for increasing hp.
+		hp = (max_hp = (hp_level * 20) + default_max_hp);
+		
+		foreach (Armor armor in inventory.active_armor_list) {
+			if (armor != null) {
+				foreach (Stat_Modifier stat_mod in armor.template.stat_mods) {
+					process_stat_mod(stat_mod);
+				}
+			}
+		}
+		foreach (Weapon weapon in inventory.active_weapon_list) {
+			if (weapon != null ) {
+				foreach (Stat_Modifier stat_mod in weapon.template.stat_mods) {
+					process_stat_mod(stat_mod);
+				}
+			}
+		}
+	}
+
+	internal void process_stat_mod(Stat_Modifier stat_mod) {
+		switch (stat_mod.stat) {
+			case Char_Stat.ARMOR:
+				armor += stat_mod.amount;
+				break;
+			case Char_Stat.CRIT:
+				crit_chance += stat_mod.amount;
+				break;
+			case Char_Stat.HP:
+				hp += stat_mod.amount;
+				break;
+			case Char_Stat.REG:
+				hp_regen += stat_mod.amount;
+				break;
+			case Char_Stat.RESI:
+				magic_resi += stat_mod.amount;
+				break;
+			case Char_Stat.TEMPO:
+				attack_tempo += stat_mod.amount;
+				break;
+		}
+	}
+
+	public void give_ability(string ability_ID, Mod_Template mod) {
+
+		Debug.Log("$$Checking inventory nullity...");
+
+		if (inventory == null) {
+			Debug.LogError("Uh oh, inventory is null?!");
+			return;
+		}
+
+		Debug.Log("$$Checking get_ability...");
+
+		if (mod.get_ability(ability_ID).min_player_level == -1) {
+			Debug.LogError("Uh oh, get_ability is returning an empty ability type?!");
+		}
+
+		Debug.Log("$$Learning the ability...");
+
+		inventory.learn_ability(ability_ID, mod.get_ability(ability_ID));
+	}
+
+	public void give_weapon(string weapon_ID, Mod_Template mod) {
+		Weapon_Template weapon = (Weapon_Template)mod.spawn_ent("weapon." + weapon_ID, transform.position);
+		inventory.pickup(weapon.game_object.GetComponent<Weapon>());
+	}
+
+	public void give_armor(string armor_ID, Mod_Template mod) {
+		Armor_Template armor = (Armor_Template)mod.spawn_ent("armor." + armor_ID, transform.position);
+		inventory.pickup(armor.game_object.GetComponent<Armor>());
+	}
+
+	public void give_material(string material_ID, Mod_Template mod) {
+		Material_Template material = (Material_Template)mod.spawn_ent("material." + material_ID, transform.position);
+		inventory.pickup(material.game_object.GetComponent<Material>());
+	}
+
+	public void give_consumable(string consumable_ID, Mod_Template mod) {
+		Consumable_Template consumable = (Consumable_Template)mod.spawn_ent("consumable." + consumable_ID, transform.position);
+		inventory.pickup(consumable.game_object.GetComponent<Consumable>());
+	}
+
+	public void give_pet_item(string pet_item_ID, Mod_Template mod) {
+		Pet_Item_Template pet_item = (Pet_Item_Template)mod.spawn_ent("pet_item." + pet_item_ID, transform.position);
+		inventory.pickup(pet_item.game_object.GetComponent<Pet_Item>());
 	}
 
 	#region Construction and Singleton
