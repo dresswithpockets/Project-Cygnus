f418d20d5f7e4085f5a3d9e90ea4be043d9d881d
Tristen Horton <tristen@tristenhorton.com>
2016-06-06 00:42:21 -0700

    Base Weapon Class

--- Assets/Scripts/API/AWeapon.cs
+++ Assets/Scripts/API/AWeapon.cs
@@ -3,4 +3,26 @@ using System.Collections;
 
 public abstract class AWeapon : AItem {
 
+	public bool m_Active = false;
+	public bool Active {
+		get {
+			return m_Active;
+		}
+		internal set {
+			m_Active = value;
+		}
+	}
+
+	private bool m_Using = false;
+	public bool Using {
+		get {
+			return m_Using;
+		}
+		set {
+			if (value && Owner != null) GameController.InvokeUsedItem(this, Owner, this);
+			m_Using = value;
+		}
+	}
+
+	public abstract WeaponHandiness Handiness { get; }
 }
