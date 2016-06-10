adc47088cb69da37edc97dd4f0061bbaaaac5b4b
Ashley <dresswithpockets@pm.me>
2016-06-10 03:12:05 -0700

    LookAt script for camera-facing sprites & labels

---
+++ Assets/LookAt.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+
+	public bool LookAtCamera;
+	public Transform Target;
+
+	void Update() {
+		if (LookAtCamera) transform.LookAt(Camera.main.transform);
+		else if (Target) transform.LookAt(Target);
+	}
+}
