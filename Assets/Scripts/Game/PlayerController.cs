09d02275a320d6f51c6624592276c12cd06eec14
Tristen Horton <tristen@tristenhorton.com>
2016-06-10 03:25:53 -0700

    Implemented health, magic, stamina, damage, and inventory handling.
    Including ability triggering, death, reviving, etc. More to come!

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -4,15 +4,42 @@ using System.Collections.Generic;
 public class PlayerController : Pawn {
 
 	#region Stats
-
-	private bool m_IsAlive = false;
+	
 	private float m_HP = 0;
-	private float m_Magic = 100;
+	private float m_MP = 0;
+	private float m_Stamina = 0;
+	private float m_Armor = 0;
+	private float m_MagicResistance = 0;
+	private float m_StaminaRegenRate = 30; // TODO: default?
+
+	private float m_MaxHealth = 0;
+	private float m_MaxMagic = 0;
+	private float m_MaxStamina = 0;
+
+	private float m_MaxHealthAdditive = 0f;
+	private float m_MaxMagicAdditive = 0f;
+	private float m_MaxStaminaAdditive = 0f;
+
+	private int m_MaxHealthLevel = 1;
+	private int m_MaxMagicLevel = 1;
+	private int m_MaxStaminaLevel = 1;
+
+	private bool m_StaminaLocked = false;
+	private float m_TimeSinceStaminaUsed = 0f;
 
 	public float PickupDistance = 7.5f;
 
-	public float MaxHealth = 100;
+	public float HealthLevelMod = 10;
+	public float StaminaLevelMod = 2;
+
 	public float MaxHealthDefault = 100;
+	public float MaxMagicDefault = 100;
+	public float MaxStaminaDefault = 100;
+
+	public float ArmorModConstant = 100f;
+	public float ResiModConstant = 100f;
+	
+	public float StaminaRegenDelay = 1.5f;
 
 	public float HP {
 		get {
@@ -20,11 +47,67 @@ public class PlayerController : Pawn {
 		}
 		set {
 			m_HP = value;
-			if (m_HP < 0f || Mathf.Approximately(m_HP, 0f)) {
-				m_HP = 0f;
-				m_IsAlive = false;
-			}
-			else m_IsAlive = true;
+			if (m_HP < 0f || Mathf.Approximately(m_HP, 0f)) m_HP = 0f;
+			else if (m_HP > m_MaxHealth) m_HP = m_MaxHealth;
+		}
+	}
+
+	public float MP {
+		get {
+			return m_MP;
+		}
+		set {
+			m_MP = value;
+			if (m_MP < 0f || Mathf.Approximately(m_MP, 0f)) m_MP = 0f;
+			else if (m_MP > m_MaxMagic) m_MP = m_MaxMagic;
+		}
+	}
+
+	public float Stamina {
+		get {
+			return m_Stamina;
+		}
+		set {
+			m_Stamina = value;
+			if (m_Stamina > m_MaxStamina) m_Stamina = m_MaxStamina;
+			//if (m_Stamina < 0f || Mathf.Approximately(m_Stamina, 0f)) m_Stamina = 0f;
+			//else if (m_Stamina > m_MaxStamina) m_Stamina = m_MaxStamina;
+		}
+	}
+
+	public float Armor {
+		get {
+			return m_Armor;
+		}
+	}
+
+	public float MagicResistance {
+		get {
+			return m_MagicResistance;
+		}
+	}
+
+	public float StaminaRegenRate {
+		get {
+			return m_StaminaRegenRate;
+		}
+	}
+
+	public float MaxHealth {
+		get {
+			return m_MaxHealth;
+		}
+	}
+
+	public float MaxMagic {
+		get {
+			return m_MaxMagic;
+		}
+	}
+
+	public float MaxStamina {
+		get {
+			return m_MaxStamina;
 		}
 	}
 
@@ -35,6 +118,12 @@ public class PlayerController : Pawn {
 	}
 
 	#endregion
+
+	public override string Name {
+		get {
+			return "LocalPlayer";
+		}
+	}
 
 	private bool m_AllowMovement = true;
 
@@ -47,15 +136,15 @@ public class PlayerController : Pawn {
 			return m_AllowMovement;
 		}
 		set {
-			if (value) m_FPController.SetState("Move");
-			else m_FPController.SetState("NoMove");
+			if (value) FPController.SetState("NoMove", false);
+			else FPController.SetState("NoMove", true);
 			m_AllowMovement = value;
 		}
 	}
 
 	public Vector3 PlayerVelocity {
 		get {
-			return m_FPController.m_Velocity;
+			return FPController.m_Velocity;
 		}
 	}
 
@@ -71,52 +160,99 @@ public class PlayerController : Pawn {
 		}
 	}
 
-	private UltimateOrbitCamera m_OrbitCamera;
-	private vp_FPInput m_FPInput;
-	private vp_FPController m_FPController;
-	private Collider m_BodyCollider;
-	private InventoryController m_Inventory;
+	public UltimateOrbitCamera OrbitCamera { get; private set; }
+	public vp_FPInput FPInput { get; private set; }
+	public vp_FPController FPController { get; private set; }
+	public Collider BodyCollider { get; private set; }
+	public InventoryController Inventory { get; private set; }
+	public PlayerUIController PlayerUI { get; private set; }
 
 	void Start() {
-		m_OrbitCamera = GetComponentInChildren<UltimateOrbitCamera>();
-		m_FPInput = GetComponent<vp_FPInput>();
-		m_FPController = GetComponent<vp_FPController>();
-		m_BodyCollider = GetComponent<Collider>();
-		m_Inventory = GetComponent<InventoryController>();
+		OrbitCamera = GetComponentInChildren<UltimateOrbitCamera>();
+		FPInput = GetComponent<vp_FPInput>();
+		FPController = GetComponent<vp_FPController>();
+		BodyCollider = GetComponent<Collider>();
+		Inventory = GetComponent<InventoryController>();
+		PlayerUI = GetComponent<PlayerUIController>();
 
-		MaxHealth = MaxHealthDefault;
+		// TODO: Update stats from save file
 
-		AllowMovement = AllowMovement; // Call the property's set routine to update the player's state.
+		SetupMaxHealth();
+		SetupMaxMagic();
+		SetupMaxStamina();
 
-		m_OrbitCamera.cameraCollision = true;
+		m_HP = m_MaxHealth;
+		m_MP = m_MaxMagic;
+		m_Stamina = m_MaxStamina;
+
+		m_TimeSinceStaminaUsed = StaminaRegenDelay;
+
+		Alive = true; // Call the property's set routine to update the player's state.
+
+		OrbitCamera.cameraCollision = true;
+
+		PlayerUI.ToggleInteractText(false);
 
 		Spawn(transform.position);
 	}
 
+	void SetupMaxHealth() {
+		m_MaxHealth = MaxHealthDefault;
+		for (int i = 0; i < m_MaxHealthLevel; i++) {
+			m_MaxHealth += HealthLevelMod * i;
+		}
+		m_MaxHealth += m_MaxHealthAdditive;
+	}
+
+	void SetupMaxMagic() {
+		m_MaxMagic = MaxMagicDefault;
+
+		m_MaxMagic += m_MaxMagicAdditive;
+	}
+
+	void SetupMaxStamina() {
+		m_MaxStamina = MaxStaminaDefault;
+		for (int i = 1; i < m_MaxStaminaLevel; i++) {
+			m_MaxStamina += StaminaLevelMod * i;
+		}
+		m_MaxStamina += m_MaxStaminaAdditive;
+	}
+
 	#region Update
 
-	void Update() {
-		if (m_IsAlive) {
-			UpdateInput();
+	public override void Update() {
+		if (Alive) {
+			UpdateAbilities();
 
 			UpdateMovement();
 
 			UpdateRotation();
+
+			UpdateInteract();
 		}
+		else if (vp_Input.GetButtonDown("Use")) { // Player wants to respawn
+			Spawn(true);
+		}
+
+		UpdateHealth();
+
+		UpdateStamina();
+
+		base.Update();
 
 		GameController.InvokeUpdate(this, this);
 	}
 
-	void UpdateInput() {
-		if (vp_Input.GetButtonDown("Use")) {
-			AItem[] nearItems = FindObjectsOfType<AItem>();
-			foreach (AItem item in nearItems) {
-				if (Util.SqrDistance(item.transform.position, transform.position) < SqrPickupDistance) {
-					PickupItem(item);
-					break;
-				}
+	void UpdateAbilities() {
+		Ability[] abilities = Inventory.Abilities;
+		foreach (Ability ability in abilities) {
+			if (ability != null && (ability.AbilityType == AbilityType.PASSIVE || ability.DoUpdate)) {
+				ability.TriggerUpdate();
 			}
 		}
+
+		// Because I'm too lazy to write four if statements. Plus, this looks cooler ;)
+		for (int i = 1; i <= 4; i++) if (vp_Input.GetButtonDown("Ability" + i.ToString())) Inventory.Abilities[i].Trigger();
 	}
 
 	void UpdateMovement() {
@@ -127,16 +263,56 @@ public class PlayerController : Pawn {
 
 	void UpdateRotation() {
 		if (MovementKeysDown) { // TODO: or casting ability or using weapon
-			Vector3 oldCamPos = m_OrbitCamera.transform.position;
-			Quaternion oldCamRot = m_OrbitCamera.transform.rotation;
+			Vector3 oldCamPos = OrbitCamera.transform.position;
+			Quaternion oldCamRot = OrbitCamera.transform.rotation;
 
 			Vector3 newEulerRot = Vector3.zero;
-			newEulerRot.y = m_OrbitCamera.transform.eulerAngles.y;
+			newEulerRot.y = OrbitCamera.transform.eulerAngles.y;
 			transform.eulerAngles = newEulerRot;
 
-			m_OrbitCamera.transform.position = oldCamPos;
-			m_OrbitCamera.transform.rotation = oldCamRot;
+			OrbitCamera.transform.position = oldCamPos;
+			OrbitCamera.transform.rotation = oldCamRot;
 		}
+	}
+
+	void UpdateHealth() {
+
+		if (Alive) {
+			if (m_HP < 0f || Mathf.Approximately(m_HP, 0f)) { // The player just died!
+				Alive = false;
+
+				PlayerUI.SetInteractText("Revive", true);
+
+				GameController.InvokeDied(this, this);
+			}
+		}
+    }
+
+	void UpdateStamina() {
+
+		if (!Alive && AliveLastFrame) Stamina = m_MaxStamina;
+		else if (Alive && !m_StaminaLocked) {
+			if (m_TimeSinceStaminaUsed < StaminaRegenDelay) m_TimeSinceStaminaUsed += Time.deltaTime;
+			else {
+				Stamina += StaminaRegenRate * Time.deltaTime;
+			}
+		}
+	}
+
+	void UpdateInteract() {
+		bool showInteract = false;
+		Item[] nearItems = FindObjectsOfType<Item>();
+		foreach (Item item in nearItems) {
+			if (item.Owner != this && Util.SqrDistance(item.transform.position, transform.position) < SqrPickupDistance) {
+				if (vp_Input.GetButtonDown("Use")) {
+					PickupItem(item);
+                    continue;
+				}
+				showInteract = true;
+				break;
+			}
+		}
+		PlayerUI.SetInteractText("Pick Up", showInteract);
 	}
 
 	void FixedUpdate() {
@@ -149,19 +325,120 @@ public class PlayerController : Pawn {
 
 	#endregion
 
-	public void PickupItem(AItem item) {
+	public void PickupItem(Item item) {
 		if (item) {
-			// Temporary picking up, specifically for weapons. Hidden inventory is nonexistent atm.
-			if (typeof(AWeapon).IsAssignableFrom(item.GetType())) {
-				m_Inventory.Pickup(item);
-				m_Inventory.Equip((AWeapon)item, (WeaponSlot.RIGHT_HAND));
+			Inventory.Pickup(item);
+			if (typeof(Weapon).IsAssignableFrom(item.GetType())) {
+				Inventory.Equip((Weapon)item, WeaponSlot.RIGHT_HAND);
 			}
 		}
 	}
 
+	public void Spawn(bool atStatue = false) {
+		GameObject[] statues;
+		if (atStatue && (statues = GameObject.FindGameObjectsWithTag("Statue")).Length > 0) {
+			int closestIndex = 0;
+			float sqrDistance = 0f;
+			for (int i = 0; i < statues.Length; i++) {
+				float newDistance = Util.SqrDistance(transform.position, statues[i].transform.position);
+				if (newDistance < sqrDistance) closestIndex = i;
+			}
+			Spawn(statues[closestIndex].transform.position + new Vector3(0f, 0f, 2f));
+		}
+		else Spawn(GameController.Instance.SpawnPoint);
+	}
+
 	public void Spawn(Vector3 pos) {
 		transform.position = pos;
-		HP = MaxHealth;
+
+		// Reset stats
+		HP = m_MaxHealth;
+		MP = m_MaxMagic;
+		Stamina = m_MaxStamina;
+
+		Alive = true;
+
+		// TODO: Roll Abilities and Skills that aren't locked in.
+
 		GameController.InvokeSpawned(this, this);
+	}
+
+	public bool UseStamina(float stamina) {
+		if (Stamina > 0f) { // Can use the stamina
+
+			//m_TimeSinceStaminaUsed = 0f; // reset the timer for stamina usage
+			m_TimeSinceStaminaUsed = 0f;
+			Stamina -= stamina;
+
+			return true; // Stamina was used, so return true
+		}
+
+		return false; // Stamina wasn't used, so return false
+	}
+
+	/// <summary>
+	/// Stamina will not regenerate unless it is unlocked.
+	/// </summary>
+	/// <param name="lockStamina">Whether to lock or unlock stamina</param>
+	public void LockStaminaRegen(bool lockStamina) {
+		m_StaminaLocked = lockStamina;
+	}
+
+	public bool UseMagic(float magic) {
+		if (MP > magic || Mathf.Approximately(MP, magic)) {
+
+			MP -= magic;
+			return true;
+		}
+
+		return false;
+	}
+	
+	public override void Damage(DamageInfo damage) {
+		
+		if (!damage.Passed) {
+			switch (damage.Element) {
+				case DamageElement.FIRE:
+
+					// TODO: Manage damage over time
+
+					break;
+				case DamageElement.ICE:
+
+					// TOOD: Manage slowing down
+
+					break;
+				case DamageElement.NONE:
+					float dmgMod = 1f;
+
+					switch (damage.Type) {
+						case DamageType.MAGIC:
+
+							dmgMod = (m_MagicResistance > 0 || Mathf.Approximately(m_MagicResistance, 0f)) ?
+								ResiModConstant / (ResiModConstant + m_MagicResistance) : // if Resi >= 0
+								2f - (ResiModConstant / (ResiModConstant - m_MagicResistance)); // otherwise
+
+							break;
+						case DamageType.PHYSICAL:
+
+							dmgMod = (m_Armor > 0 || Mathf.Approximately(m_Armor, 0f)) ?
+								ArmorModConstant / (ArmorModConstant + m_Armor) : // if Armor >= 0
+								2f - (ArmorModConstant / (ArmorModConstant - m_Armor)); // otherwise
+
+							break;
+					}
+
+					HP -= dmgMod * damage.Damage;
+
+					base.Damage(damage);
+
+					DebugConsole.Log(FullName + ".HP = " + HP.ToString());
+
+					break;
+			}
+		}
+		else {
+			// TODO: Damage over time
+		}
 	}
 }
