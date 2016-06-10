74b08d5cb2352a2e33f7d4e8b79c70df0144209a
Ashley <dresswithpockets@pm.me>
2016-06-10 03:24:31 -0700

    Simple weapon handling, with event invoking in GC

---
+++ Assets/Scripts/API/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Weapon : Item {
+
+	public bool m_Active = false;
+	public bool Active {
+		get {
+			return m_Active;
+		}
+		internal set {
+			m_Active = value;
+		}
+	}
+
+	private bool m_Using = false;
+	public bool Using {
+		get {
+			return m_Using;
+		}
+		set {
+			if (m_Using != value && value && Owner != null) GameController.InvokeUsedItem(this, Owner, this);
+			m_Using = value;
+		}
+	}
+
+	public abstract WeaponHandiness Handiness { get; }
+}
