addea472ae76a4ab83db82ce3d155df349840d27
Ashley <dresswithpockets@pm.me>
2016-06-10 03:20:49 -0700

    Changed how FullNames work; got rid of boilerplate property.
    FullNames now use actual BaseType names instead of simply "AItem". I
    also decided that using AutoProperties would be acceptable for private
    fields as well as internally-set fields (Owner is a good example).

---
+++ Assets/Scripts/API/Item.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public abstract class Item : MonoBehaviour {
+
+	public Pawn Owner { get; internal set; }
+
+	public bool IsEquippable {
+		get {
+			foreach (Type t in GameController.EquippableTypes)
+				if (t.IsAssignableFrom(GetType())) return true;
+
+			return false;
+		}
+	}
+
+	public void ToggleItem(bool toggle) {
+
+		Renderer[] itemRenderers = GetComponentsInChildren<Renderer>();
+		Collider[] itemColliders = GetComponentsInChildren<Collider>();
+
+		foreach (Renderer r in itemRenderers) {
+			r.enabled = toggle;
+		}
+
+		foreach (Collider c in itemColliders) {
+			c.enabled = toggle;
+		}
+	}
+
+	/// <summary>
+	/// Returns a BaseType-lead name of the item. Example: [AItem:Water Bottle]
+	/// </summary>
+	public string FullName {
+		get {
+			return "[" +
+				(GetType().BaseType.Equals(typeof(MonoBehaviour)) ? GetType().Name : GetType().BaseType.Name) +
+				":" + Name + "]";
+		}
+	}
+
+	public virtual string Name {
+		get {
+			return "Default" + GetType().Name;
+		}
+	}
+
+	public virtual void Start() {
+
+	}
+
+	public virtual void Update() {
+
+	}
+}
