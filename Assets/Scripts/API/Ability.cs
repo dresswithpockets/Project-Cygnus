dc3d169f28c86961d790140815dfbe9b15a4acfa
Tristen Horton <tristen@tristenhorton.com>
2016-06-10 03:18:48 -0700

    Base ability functionality, including event invoking in the GameController

---
+++ Assets/Scripts/API/Ability.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Ability {
+
+	public Pawn Owner { get; internal set; }
+
+	public bool DoUpdate { get; private set; }
+
+	public string FullName {
+		get {
+			return "[" +
+				(GetType().BaseType.Equals(typeof(MonoBehaviour)) ? GetType().Name : GetType().BaseType.Name) +
+				":" + Name + "]";
+		}
+	}
+
+	public abstract string Name { get; }
+
+	public virtual AbilityType AbilityType {
+		get {
+			return AbilityType.ACTIVE;
+		}
+	}
+
+	/// <summary>
+	/// If AbilityType is AbilityType.PASSIVE then Trigger is called when a player equips this ability.
+	/// Otherwise, Trigger is called when a player triggers the ability.
+	/// For non-casted abilities, it is expected that the entire ability is handled here.
+	/// </summary>
+	public virtual void Trigger() { GameController.InvokeTriggeredAbility(this, Owner, this); }
+
+	/// <summary>
+	/// TriggerUpdate is called once a frame when DoUpdate is true or if AbilityType is AbilityType.PASSIVE.
+	/// For casted abilities, it is expected that the cast-time and the cast is handled here.
+	/// </summary>
+	public virtual void TriggerUpdate() { }
+
+	/// <summary>
+	/// Called when an event occurs on the Owner that invokes an ability Interrupt.
+	/// For casted abilities, it is expected that this stops any ability from casting/being used.
+	/// </summary>
+	public virtual void Interrupt() { }
+}
