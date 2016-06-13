f896d80e3ee265d30a3a30683135c0027be3f170
Tristen Horton <tristen@tristenhorton.com>
2016-06-12 22:31:35 -0700

    Auto-incrementing build number support

--- Assets/Scripts/API/Util.cs
+++ Assets/Scripts/API/Util.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Reflection;
 using System.Collections.Generic;
+using PicaVoxel;
+using System.IO;
 
 [assembly: AssemblyVersion("0.1.*")]
 public static class Util {
@@ -25,10 +27,61 @@ public static class Util {
 		}
 	}
 
-	public static string AssemblyVersion {
+	private static int m_BuildNumber;
+	public static int BuildNumber {
+		get {
+			if (m_BuildNumber == 0) m_BuildNumber = BitConverter.ToInt32(File.ReadAllBytes("version"), 0);
+			return m_BuildNumber;
+		}
+	}
+
+	public static Version AssemblyVersion {
 		get {
 
-			return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+			return Assembly.GetExecutingAssembly().GetName().Version;
+		}
+	}
+
+	public static byte[] ReadToEnd(System.IO.Stream stream) {
+		long originalPosition = 0;
+
+		if (stream.CanSeek) {
+			originalPosition = stream.Position;
+			stream.Position = 0;
+		}
+
+		try {
+			byte[] readBuffer = new byte[4096];
+
+			int totalBytesRead = 0;
+			int bytesRead;
+
+			while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0) {
+				totalBytesRead += bytesRead;
+
+				if (totalBytesRead == readBuffer.Length) {
+					int nextByte = stream.ReadByte();
+					if (nextByte != -1) {
+						byte[] temp = new byte[readBuffer.Length * 2];
+						Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+						Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+						readBuffer = temp;
+						totalBytesRead++;
+					}
+				}
+			}
+
+			byte[] buffer = readBuffer;
+			if (readBuffer.Length != totalBytesRead) {
+				buffer = new byte[totalBytesRead];
+				Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+			}
+			return buffer;
+		}
+		finally {
+			if (stream.CanSeek) {
+				stream.Position = originalPosition;
+			}
 		}
 	}
 
