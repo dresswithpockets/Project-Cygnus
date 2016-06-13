4451ee1dccf98c29a0519cc3949a4fce047111e7
Ashley <dresswithpockets@pm.me>
2016-06-12 22:20:28 -0700

    Base plugin for testing new plugin-based features

---
+++ Assets/Scripts/Game/BasePlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public sealed class BasePlugin : Plugin {
+	public override string Name {
+		get {
+			return "Base";
+		}
+	}
+
+	public override string Author {
+		get {
+			return "***REMOVED***";
+		}
+	}
+
+	public override Version Version {
+		get {
+			return Util.AssemblyVersion;
+		}
+	}
+
+	public override void Initialize() {
+		LoadModel("ShortSword", "ShortSword");
+
+		SpawnItem(typeof(ShortSword), new Vector3(0f, 2f, 0f), Quaternion.identity, "ShortSword");
+	}
+
+	public override void Update() {
+		
+	}
+}
