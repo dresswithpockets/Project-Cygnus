8d42b11c909109c86a4f0cbedb4e8db59baa0d87
Ashley <dresswithpockets@pm.me>
2016-06-12 22:20:13 -0700

    Added auto-incrementing Build Number
    ...for eventual build-specific releases.

---
+++ Assets/Scripts/Editor/BuildVersion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.IO;
+using System;
+
+[InitializeOnLoad]
+public class BuildVersion {
+
+	static BuildVersion() {
+		EditorApplication.update += RunOnce;
+	}
+
+	static void RunOnce() {
+		EditorApplication.update -= RunOnce;
+		ReadVersionAndIncrement();
+	}
+
+	static void ReadVersionAndIncrement() {
+		if (!File.Exists("version")) {
+			using (FileStream fs = File.Create("version")) {
+				byte[] v = BitConverter.GetBytes(1);
+
+				for (int i = 0; i < v.Length; i++) fs.WriteByte(v[i]);
+			}
+			return;
+		}
+
+		using (FileStream fs = File.Open("version", FileMode.Open)) {
+			byte[] newBuildVersion = BitConverter.GetBytes(BitConverter.ToInt32(Util.ReadToEnd(fs), 0) + 1);
+			fs.Write(newBuildVersion, 0, 4);
+		}
+	}
+}
