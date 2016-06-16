836670812c336dfc6c3df38c63de740792a34f64
Tristen Horton <tristen@tristenhorton.com>
2016-06-15 17:36:02 -0700

    Revert "Revert "Added minimal dependencies from UFPS for CI""
    This reverts commit 767cf81a58cde3b2aa6018e85cb0629afdc838b2.

---
+++ Assets/UFPS/Base/Scripts/Gameplay/vp_Gameplay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class vp_Gameplay
+{
+
+	public static bool isMultiplayer = false;
+	protected static bool m_IsMaster = true;
+	public static bool isMaster
+	{
+		get
+		{
+			if (!isMultiplayer)	// always true in singleplayer
+				return true;
+			return m_IsMaster;
+		}
+		set
+		{
+			if (!isMultiplayer)	// can't be set in singleplayer
+				return;
+			m_IsMaster = value;
+		}
+	}
+
+}
