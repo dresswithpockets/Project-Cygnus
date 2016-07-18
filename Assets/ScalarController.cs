8ba3fb924a92a39fd7bffdbf2dbd51a3acf0e531
Ashley <dresswithpockets@pm.me>
2016-07-18 15:35:35 -0700

    Added some ufps dependencies for compiling.

---
+++ Assets/ScalarController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalarController : MonoBehaviour {
+
+	bool increaseScale = true;
+	
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < transform.childCount; i++) transform.GetChild(i).Translate(new Vector3(0.05f * i, .05f * i, 0f));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.S)) {
+
+			if (increaseScale) transform.localScale = new Vector3(2f, 2f, 2f);
+			else transform.localScale = new Vector3(1f, 1f, 1f);
+
+			increaseScale = !increaseScale;
+		}
+	}
+}
---
+++ Assets/UFPS/Base/Scripts/Core/Editor/vp_EditorApplicationQuit.cs
@@ -0,0 +1,27 @@
+﻿/////////////////////////////////////////////////////////////////////////////////
+//
+//	vp_EditorApplicationQuit.cs
+//	© VisionPunk. All Rights Reserved.
+//	https://twitter.com/VisionPunk
+//	http://www.visionpunk.com
+//
+//	description:	this is a small convenience feature allowing the game to quit
+//					itself when running in the editor by means of vp_GlobalEvent
+//					"EditorApplicationQuit"
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class vp_EditorApplicationQuit : Editor
+{
+
+	static vp_EditorApplicationQuit()
+	{
+		vp_GlobalEvent.Register("EditorApplicationQuit", () => { EditorApplication.isPlaying = false; });
+	}
+			
+}
+
